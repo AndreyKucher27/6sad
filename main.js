83356f6a3d7c3eb5309eaef2647bfc449650da62
AndreyKucher27 <144955355+AndreyKucher27@users.noreply.github.com>
2023-12-13 21:22:13 +0300

    Update main.js

--- main.js
+++ main.js
@@ -9,54 +9,54 @@ const errormeselem = document.getElementById("errorMessage");
 const prices = { service1: 10, service2: 15, service3: 20 };
 const pricesprod = { product1: 200, product2: 300, product3: 400 };
 
-function itog() {
+function  calculateTotal() {
     const chosenserv = document.querySelector('input[name="service"]:checked').value;
     const kolvo = kolvoInput.value;
-
-    if (kolvo < 0) {
+//проверка значения количества
+    if (kolvo < 0) { 
         errormeselem.textContent = "Ошибка ввода: введите положительное число.";
         return;
-    } else {
+    } else { 
         errormeselem.textContent = "";
     }
 
-    if (chosenserv) {
+    if (chosenserv) { //выбираем услугу
         let total = 0;
 
-        if (chosenserv === "service2") {
+        if (chosenserv === "service2") { //если выбрали вторую услугу
             const selectedProduct = prodoptions.querySelector("select").value;
             const productPrice = pricesprod[selectedProduct];
             total = prices[chosenserv] * kolvo + productPrice;
-        } else if (chosenserv === "service3") {
-            total = dopprops.querySelector("input").checked
-                ? 2 * prices[chosenserv] * kolvo
+        } else if (chosenserv === "service3") { //если выбрали третью услугу
+            total = dopprops.querySelector("input").checked //если кнопку нажали, умножаем на 2, иначе не умножаем
+                ? 3 * prices[chosenserv] * kolvo
                 : prices[chosenserv] * kolvo;
         } else {
-            total = prices[chosenserv] * kolvo;
+            total = prices[chosenserv] * kolvo; //1 услуга, просто умножаем цену на кол-во
         }
 
         finalcost.textContent = total;
     }
 
-    if (chosenserv === "service2") {
+    if (chosenserv === "service2") { //если выбираем вторую услугу, то prodoptions отображается, а dopprops скрывается
         prodoptions.style.display = "block";
         dopprops.style.display = "none";
-    } else if (chosenserv === "service3") {
+    } else if (chosenserv === "service3") { //если выбираем третью услугу, то prodoptions скрывается, а dopprops отображается
         prodoptions.style.display = "none";
         dopprops.style.display = "block";
-    } else {
+    } else { //иначе скрываем все на отображение первой услуги
         prodoptions.style.display = "none";
         dopprops.style.display = "none";
     }
 }
 
-kolvoInput.addEventListener("input", itog);
+kolvoInput.addEventListener("input",  calculateTotal); //кнопка сброса
 kolvobutton.addEventListener("click", () => {
     kolvoInput.value = "";
     errormeselem.textContent = "";
 });
-servradio.forEach((radio) => radio.addEventListener("change", itog));
-dopprops.querySelector("input").addEventListener("change", itog);
-prodoptions.querySelector("select").addEventListener("change", itog);
+servradio.forEach((radio) => radio.addEventListener("change",  calculateTotal)); //при нажатии на кнопку
+dopprops.querySelector("input").addEventListener("change",  calculateTotal); //введение кол-ва в калькулятор и изменение итоговой стоимости
+prodoptions.querySelector("select").addEventListener("change",  calculateTotal); //выбор одной из трех услуг
 
-itog();
+itog(); //вызов
